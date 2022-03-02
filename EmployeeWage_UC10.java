d7047dc01612b0465bb355996c2dc8335dbf017b
kumarb97 <kumarbamankar@gmail.com>
2022-03-02 12:10:41 +0530

    use case 10 added

---
+++ EmployeeWage_UC10.java
@@ -0,0 +1,37 @@
+package employeewage1;
+
+public class EmployeeWage_UC10 {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		boolean loop = true;
+		int ch;
+		
+		CompanyService obj = new CompanyService();
+		
+		while (loop) {
+			System.out.println("\n ======== ");
+			System.out.println(" 1. Add company\n 2. Print Employee Details\n 3. Exit");
+			System.out.print(" Please enter your choice: ");
+			ch = scan.nextInt();
+			
+			switch(ch) {
+			case 1:
+				obj.addCompany();
+				break;
+			case 2:
+				obj.printCompany();
+				break;
+			case 3:
+				return;
+			default: 
+				System.out.println(" Please enter valid input");
+				break;
+			}
+		}
+		
+		scan.close();		
+
+	}
+
+}
